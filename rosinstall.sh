1b3f831ffe82e007f18f9ef50926a5fc294fa3a2
Dmitriy Afanasev <main2stels@gmail.com>
2023-09-15 18:13:37 +0300

    Update rosinstall.sh

--- rosinstall.sh
+++ rosinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo INSTALL ROS 
 apt-get install ros-noetic-mavros ros-noetic-mavros-extras ros-noetic-mavros-msgs -y
 apt-get install ros-noetic-image-geometry -y
 apt-get install ros-noetic-resource-retriever -y
@@ -6,10 +7,13 @@ apt-get install ros-noetic-resource-retriever -y
 wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
 bash ./install_geographiclib_datasets.sh
 
+echo CLONE GRIDBOARD
 cd /home/ubuntu/catkin_ws/src/
 git clone https://github.com/AlexandrShipovsky/aruco_gridboard.git
 
 cd -
+
+echo $PWD CP FILES
 
 rm /opt/ros/noetic/share/mavros/launch/px4_pluginlists.yaml
 rm /opt/ros/noetic/share/mavros/launch/apm_config.yaml
@@ -18,6 +22,10 @@ cp $PWD/mavros_launch/px4_pluginlists.yaml /opt/ros/noetic/share/mavros/launch/p
 cp $PWD/mavros_launch/apm_config.yaml /opt/ros/noetic/share/mavros/launch/apm_config.yaml
 
 cd /home/ubuntu/catkin_ws/
+
+
+echo MAKE
+
 catkin_make
 
 apt install python-is-python3
@@ -25,7 +33,13 @@ pip3 install pymavlink
 
 apt-get install ros-noetic-robot-upstart -y
 
+echo SLEEP_10
+
+sleep 10
+
 rosrun robot_upstart install aruco_gridboard/launch/detection_rpicam.launch
+
+cd -
 
 cp -f $PWD/aruco.service /etc/systemd/system/multi-user.target.wants/aruco.service
 
