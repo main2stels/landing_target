fa2d1462232a6225a37b4b7b1651c6dfa0b25b7d
WIN-VAAGPGCG57C\user <main2stels@gmail.com>
2023-09-15 16:27:24 +0300

    update

--- rosinstall.sh
+++ rosinstall.sh
@@ -18,11 +18,17 @@ cp $PWD/mavros_launch/apm_config.yaml /opt/ros/noetic/share/mavros/launch/apm_co
 cd /home/ubuntu/catkin_ws/
 catkin_make
 
+apt install python-is-python3
+pip3 install pymavlink
+
 apt-get install ros-noetic-robot-upstart -y
 
 rosrun robot_upstart install aruco_gridboard/launch/detection_rpicam.launch
 
 cp -f $PWD/aruco.service /etc/systemd/system/multi-user.target.wants/aruco.service
+
+cp -f /home/ubuntu/catkin_ws/src/aruco_gridboard/data/camerav1_1280x720.yaml /home/ubuntu/.ros/camera_info/camerav1_1280x720.yaml
+cp -f /home/ubuntu/catkin_ws/src/aruco_gridboard/data/camerav1_1280x720.yaml /opt/ros/noetic/share/raspicam_node/camera_info/camerav1_1280x720.yaml
 
 systemctl daemon-reload
 systemctl start aruco
