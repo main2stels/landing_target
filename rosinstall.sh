a647c5ac1cfc42e0f5e41a5091f512fd40b51af5
Dmitriy Afanasev <main2stels@gmail.com>
2024-01-31 11:49:17 +0300

    Update rosinstall.sh

--- rosinstall.sh
+++ rosinstall.sh
@@ -45,7 +45,7 @@ cd -
 
 cp -f $PWD/aruco.service /etc/systemd/system/multi-user.target.wants/aruco.service
 
-cp -f /home/ubuntu/catkin_ws/src/aruco_gridboard/data/camerav1_1280x720.yaml /opt/ros/noetic/share/raspicam_node/camera_info/camerav1_1280x720.yaml
+cp -f /home/ubuntu/catkin_ws/src/aruco_gridboard/data/camerav1_640x480.yaml /opt/ros/noetic/share/raspicam_node/camera_info/camerav1_640x480.yaml
 
 systemctl daemon-reload
 systemctl start aruco
